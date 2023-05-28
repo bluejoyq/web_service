f1282d35c48ce4d764775f76314ace7d38b88a05
bluejoyq <bluejoy32@gmail.com>
2023-05-28 22:21:21 +0900

    feat: post detail view

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path(r'', views.post_list, name='post_list')
+    path(r'', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
